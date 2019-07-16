d29eabbb8fdb508b420e4c6cd95bf629100362ab
zhangweiping <zhangweiping@mudu.tv>
2019-07-16 09:52:06 +0800

    @modify 修改和删除收货地址

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,6 @@
 
 window._ = require('lodash');
-
+require('sweetalert');
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -11,6 +11,7 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
+
 } catch (e) {}
 
 /**
