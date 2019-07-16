295c0a71b4ac6cbd62ecbcd77e9b05e2919824b2
zhangweiping <zhangweiping@mudu.tv>
2019-07-16 17:16:35 +0800

    @modify 引入支付库

---
+++ config/pay.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mr.Zhangwp
+ * Date: 2019/7/16
+ * Time: 17:14
+ * Nerver Stop Learning.
+ */
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
