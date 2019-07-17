a891d1a2a0e7d2d451ec27a0730d97483e6ba07a
zhangweiping <zhangweiping@mudu.tv>
2019-07-17 16:03:10 +0800

    @modify 用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::group(['middleware' => 'auth', 'verified'], function() {
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
-
+        Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
     });
     // 结束
